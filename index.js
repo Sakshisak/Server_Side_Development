cbbc3e45ae9aa0e84ff659f4c81a5de4f3dd05da
Sakshisak <sakshisa@iitk.ac.in>
2020-04-15 03:06:33 +0530

    Mongoose 3

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'} 
+        },{
+            new:true
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
     })
     .then((dishes) => {
         console.log(dishes);
