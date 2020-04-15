dc90da60cec5dd7663c754300c071c2dea84acb6
Sakshisak <sakshisa@iitk.ac.in>
2020-04-15 16:31:04 +0530

    Express REST API with MongoDB and Mongoose Part 3

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const Dishes = require('./models/dishes');
+const Promotions = require('./models/promotions');
+const Leaders = require('./models/leaders');
 
 var app = express();
 
