5eae07951d2ea34180aa1407d0958fafb65d5d38
Sakshisak <sakshisa@iitk.ac.in>
2020-04-17 01:35:18 +0530

    Passport JWT

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 
 var indexRouter = require('./routes/index');
@@ -21,7 +22,7 @@ const Leaders = require('./models/leaders');
 
 var app = express();
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log("Connected correctly to server");
@@ -35,20 +36,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
+
+//Sessions no longer required
+/*app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
-}));
+}));*/
 
 app.use(passport.initialize());
-app.use(passport.session());
-
+//app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//Every page doesn't require authentication. So we'll add authentication only for some requests in the routes
+/*
 function auth (req, res, next) {
   console.log(req.user);
 
@@ -63,7 +67,7 @@ function auth (req, res, next) {
 }
 
 
-app.use(auth);
+app.use(auth);*/
 
 //After this everything needs to be autherized
 
@@ -77,27 +81,6 @@ app.use('/leaders',leaderRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-//auth function
-// function auth (req, res, next) {
-//   console.log(req.session);
-
-//   if(!req.session.user) {
-//       var err = new Error('You are not authenticated!');
-//       err.status = 403;
-//       return next(err);
-//   }
-//   else {
-//     if (req.session.user === 'authenticated') {
-//       next();
-//     }
-//     else {
-//       var err = new Error('You are not authenticated!');
-//       err.status = 403;
-//       return next(err);
-//     }
-//   }
-// }
 
 // error handler
 app.use(function(err, req, res, next) {
--- authenticate.js
+++ authenticate.js
@@ -1,9 +1,44 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
+//Using tokens------
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+var config = require('./config.js');
+//-----------
 
 passport.use(new LocalStrategy(User.authenticate())); //Authentication done with the username and password provided in the json body of the incoming request 
 
 //serializeUser and deserializeUser are provided in the passport User model
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//Using tokens-----
+exports.getToken = function(user) {
+    return jwt.sign(user, config.secretKey, //user is the payload
+        {expiresIn: 3600}); //3600 seconds later the token becomes invalid
+};
+
+var opts = {}; //Options for jwt Strategy
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts,
+    (jwt_payload, done) => {                                //done is the callback
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            else if (user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
